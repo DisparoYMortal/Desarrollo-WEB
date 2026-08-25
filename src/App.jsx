10b058176938e4387128433fb2baf94ad2398651
Disparo y Mortal <ls.ulisesalarcon@gmail.com>
2026-08-25 10:25:38 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ListaServicios from './components/ListaServicios'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Proyecto Capstone</h1>
+      <ListaServicios />
+    </main>
+  );
+}
+
+export default App;
